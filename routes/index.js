7b0c8ec4183c3be11e3e16702116027c229deec2
sarfophil <philsarfogh@gmail.com>
2020-03-31 20:37:49 -0500

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,8 @@ var router = express.Router();
 
 
 
-router.post('/upload', function(req, res,next) {
-  if (!req.files || Object.keys(req.files).length === 0) {
-  //  return res.status(400).send('No files were uploaded.');
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
-  sampleFile.name = 'philip.jpg'
-  console.log(sampleFile)
-
-  // Use the mv() method to place the file somewhere on your server
-  // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
-  //   if (err)
-  //     return res.status(500).send(err);
-
-  //   res.send('File uploaded!');
-  // });
-  res.send('ll')
+router.post('/', function(req, res,next) {
+  res.status(200).send('Social Network Webservice {}')
 });
 
 module.exports = router;
